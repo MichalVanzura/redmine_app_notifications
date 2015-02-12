bd9984853e2551585bb466099e6160a427f757a5
MichalVanzura <vanzura.michal@gmail.com>
2015-02-12 12:51:47 +0100

    Update init.rb
    Change author surname

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 Redmine::Plugin.register :redmine_app_notifications do
   name 'Redmine App Notifications plugin'
-  author 'Michal Vanžura'
+  author 'Michal Vanzura'
   description 'App notifications plugin provides simple in application notifications. It can replace default e-mail notifications.'
   version '0.0.1'
   url 'https://github.com/MichalVanzura/redmine_app_notifications'
