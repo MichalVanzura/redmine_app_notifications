9883f98de08979ae48873be14656ee7892769258
Androc <sdourdin@gmail.com>
2015-02-12 15:47:07 +0100

    Add option to use desktop notifications

---
+++ db/migrate/003_update_users2.rb
@@ -0,0 +1,13 @@
+class UpdateUsers2 < ActiveRecord::Migration
+    def self.up
+        change_table :users do |t|
+            t.column :app_notification_desktop, :boolean, :default => 0
+        end
+    end
+
+    def self.down
+        change_table :users do |t|
+            t.remove :app_notification_desktop
+        end
+    end
+end
