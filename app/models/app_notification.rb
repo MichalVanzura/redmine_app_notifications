ab6ffc6752ca8c4690ade5c1cbd8e4067cad85ab
Androc <sdourdin@gmail.com>
2015-06-19 12:00:06 +0200

    Adding user avatars display (#19)

--- app/models/app_notification.rb
+++ app/models/app_notification.rb
@@ -1,3 +1,6 @@
+include GravatarHelper::PublicMethods
+include ERB::Util
+
 class AppNotification < ActiveRecord::Base
 	belongs_to :recipient, :class_name => 'User', :foreign_key => 'recipient_id'
 	belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
@@ -7,7 +10,7 @@ class AppNotification < ActiveRecord::Base
 	def deliver
 		unless Setting.plugin_redmine_app_notifications['faye_server_adress'].empty?
 			channel = "/notifications/private/#{recipient.id}"
-			message = {:channel => channel, :data => { count: AppNotification.where(recipient_id: recipient.id, viewed: false).count, message: message_text, id: id}}
+			message = {:channel => channel, :data => { count: AppNotification.where(recipient_id: recipient.id, viewed: false).count, message: message_text, id: id, avatar: gravatar_url(author.mail, { :default => Setting.gravatar_default })}}
 			uri = URI.parse(Setting.plugin_redmine_app_notifications['faye_server_adress'])
 			Net::HTTP.post_form(uri, :message => message.to_json)
 		end
